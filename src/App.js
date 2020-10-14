5cb563e72d5ec44012bf180876571b244c3f1df9
SICKBOYRARI <jediz0rrr@gmail.com>
2020-10-15 02:30:12 +0700

    Added PizzaBlock component + axios + prop-types

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from "./pages";
 
 function App() {
+    const [pizzas, setPizzas] = React.useState([]);
 
-  return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route path="/" component={Home} exact />
-          <Route path="/cart" component={Cart} exact />
+    React.useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({ data }) => {
+            setPizzas(data.pizzas);
+        })
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <Route path="/" render={() => <Home items={pizzas} />} exact />
+                <Route path="/cart" component={Cart} exact />
+            </div>
         </div>
-      </div>
-  );
+    )
 }
 
 export default App;
